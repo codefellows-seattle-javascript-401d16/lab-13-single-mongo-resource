1adc44bfa9f29b240f0e728010ec248bde4ccf3a
Kyle Aardal <aardal.kyle@gmail.com>
2017-06-28 14:07:21 -0700

    nike shop model

--- lab-kyle/lib/server.js
+++ lab-kyle/lib/server.js
@@ -9,8 +9,14 @@ mongoose.Promise = Promise;
 mongoose.connect(process.env.MONGODB_URI);
 
 const app = express();
+
 app.use(cors());
 app.use(morgan('dev'));
+
+app.all('/api/*', (req, res, next) => {
+  res.sendStatus(404);
+});
+app.use(require('./error-middleware.js'));
 
 const server = module.exports = {};
 
@@ -18,16 +24,15 @@ server.isOn = false;
 
 server.start = () => {
   return new Promise((resolve, reject) => {
-    if(!server.http || !server.isOn) {
+    if(!server.isOn) {
       server.http = app.listen(process.env.PORT, () => {
         console.log('server up', process.env.PORT);
-
         server.isOn = true;
         resolve();
       });
       return;
     }
-    reject();
+    reject(new Error('server already running'));
   });
 };
 
@@ -36,12 +41,11 @@ server.stop = () => {
     if(server.http && server.isOn) {
       server.http.close(() => {
         console.log('server down');
-
         server.isOn = false;
         resolve();
       });
       return;
     }
-    reject();
+    reject(new Error('server not running'));
   });
 };
