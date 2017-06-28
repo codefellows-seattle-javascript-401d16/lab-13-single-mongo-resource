b52145ccc5483a69aa4772e129f1ce7a17d268e9
Spencer Gietzen <Speng1516@comcast.net>
2017-06-28 13:57:55 -0700

    fixed routes a tiny bit, added pagerizing(?) nd limting to get get-all route

--- lab-spencer/route/nhl-team-router.js
+++ lab-spencer/route/nhl-team-router.js
@@ -1,9 +1,10 @@
 'use strict';
 
 const jsonParser = require('body-parser').json();
-const Team = require('../model/nhl-team.js');
 
-const teamRouter = module.exports = require('express').Router;
+const teamRouter = module.exports = new require('express').Router;
+
+const Team = require('../model/nhl-team.js');
 
 teamRouter.post('/api/nhl/teams', jsonParser, (req, res, next) => {
   new Team(req.body)
@@ -12,8 +13,23 @@ teamRouter.post('/api/nhl/teams', jsonParser, (req, res, next) => {
     .catch(next);
 });
 
+teamRouter.get('/api/nhl/teams', (req, res, next) => {
+  let pageNum = Number(req.params.pageNum) || 1;
+  let numPer = Number(req.params.numPer) || 50;
+  if(pageNum < 1) pageNum = 1;
+  if(numPer < 1) numPer = 1;
+  pageNum--;
+  Team.find({})
+    .sort({name: 'asc'})
+    .skip(pageNum * numPer)
+    .limit(numPer)
+    .then(teams => res.json(teams))
+    .catch(next);
+});
+
 teamRouter.get('/api/nhl/teams/:id', (req, res, next) => {
   Team.findById(req.params.id)
+    // .populate('players')
     .then(team => res.json(team))
     .catch(next);
 });
