dd8a7a814f3e37865e6fd7de8c57c7fc433f4784
Scott <sdmccoy@gmail.com>
2017-06-28 17:57:55 -0700

    test created for POST

--- lab13-scott/test/resort-route-test.js
+++ lab13-scott/test/resort-route-test.js
@@ -1,16 +1,34 @@
 'use strict';
 
 require('dotenv').config({path: `${__dirname}/../.test.env`});
+const superagent = require('superagent');
 const expect = require('expect');
 const server = require('../server.js');
-require('./lib/mock-resort.js');
+const MockResort = require('./lib/mock-resort.js');
+const clearDB = require('./lib/clear-db.js');
 const API_URL = process.env.API_URL;
 
 describe('Testing for /api/resort routes', () => {
   before(server.start);
   after(server.stop);
-  beforeEach(() => {
-    //create new resort
+  afterEach(clearDB);
 
-  })
+  describe('Testing POST routes', () => {
+    describe('If successful', () => {
+      it('it should create a new resort and respond with 200', () => {
+        return superagent.post(`${API_URL}/api/resorts`)
+        .send(MockResort.createOne)
+        .then(res => {
+          console.log('res.body: ', res.body);
+          expect(res.status).toEqual(200);
+          expect(res.body.name).toExist();
+          expect(res.body._id).toExist();
+        });
+      });
+    });
+  });
+
+
+
+
 });
