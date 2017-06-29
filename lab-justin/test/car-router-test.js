435c7d1d9c33f1550d89b7305bd5285a14d1c4f8
Justin Green <justin98366@gmail.com>
2017-06-29 16:59:45 -0700

    complete

--- lab-justin/test/car-router-test.js
+++ lab-justin/test/car-router-test.js
@@ -3,13 +3,15 @@
 require('dotenv').config({path: `${__dirname}/../.test.env`});
 
 // npm modules
+const faker = require('faker');
 const expect = require('expect');
 const superagent = require('superagent');
 
 // app modules
-const Car = require('../model/car.js');
+// const Car = require('../model/car.js');
 const mockCar = require('./lib/mock-car.js');
 const server = require('../lib/server.js');
+const clearDB = require('./lib/clear-db.js');
 
 
 let tempCar;
@@ -18,25 +20,28 @@ const API_URL = process.env.API_URL;
 describe('testing car router', () => {
   before(server.start);
   after(server.stop);
+  after(clearDB);
 
   describe('testing POST /api/cars', () => {
-    after(() => Car.remove({}));
+    // after(() => Car.remove({}));
 
     let data = {
       make: 'Ford',
-      model: 'Mustang',
-      year: '2017',
+      model: faker.random.word(),
+      year: faker.random.number(4),
     };
 
     it('should respond with a car and 200 status', () => {
       return superagent.post(`${API_URL}/api/cars`)
       .send(data)
       .then(res => {
+        console.log('data', data);
         expect(res.status).toEqual(200);
         expect(res.body._id).toExist();
         expect(res.body.make).toEqual(data.make);
         expect(res.body.model).toEqual(data.model);
         expect(res.body.year).toEqual(data.year);
+        expect(res.body.drivers).toEqual([]);
       });
     });
 
@@ -48,32 +53,32 @@ describe('testing car router', () => {
       });
     });
 
-    // 400 because no content
-    it('should respond with a 400', () => {
-      return superagent.post(`${API_URL}/api/cars`)
-      .send({make: 'kjsdflj', year: '2017'})
-      .catch(res => {
-        expect(res.status).toEqual(400);
-      });
-    });
-
-    // 400 because no title
-    it('should respond with a 400', () => {
-      return superagent.post(`${API_URL}/api/cars`)
-      .send({model: 'kjsdfljslfkjlsdfkj', year: '2017'})
-      .catch(res => {
-        expect(res.status).toEqual(400);
-      });
-    });
-
-    // 400 because content length is < 10
-    it('should respond with a 400', () => {
-      return superagent.post(`${API_URL}/api/cars`)
-      .send({make: 'hello ', model: 'ha', year: 'year'})
-      .catch(res => {
-        expect(res.status).toEqual(400);
-      });
-    });
+    // // 400 because no content
+    // it('should respond with a 400', () => {
+    //   return superagent.post(`${API_URL}/api/cars`)
+    //   .send({make: 'kjsdflj', year: '2017'})
+    //   .catch(res => {
+    //     expect(res.status).toEqual(400);
+    //   });
+    // });
+    //
+    // // 400 because no title
+    // it('should respond with a 400', () => {
+    //   return superagent.post(`${API_URL}/api/cars`)
+    //   .send({model: 'kjsdfljslfkjlsdfkj', year: '2017'})
+    //   .catch(res => {
+    //     expect(res.status).toEqual(400);
+    //   });
+    // });
+    //
+    // // 400 because content length is < 10
+    // it('should respond with a 400', () => {
+    //   return superagent.post(`${API_URL}/api/cars`)
+    //   .send({make: 'hello ', model: 'ha', year: 'year'})
+    //   .catch(res => {
+    //     expect(res.status).toEqual(400);
+    //   });
+    // });
 
     // 409 because it has the same title twice and the model says it should
     // be unique
@@ -86,104 +91,84 @@ describe('testing car router', () => {
     });
   });
 
-  describe('testing GET /api/cars/:id', () => {
-    var tempCar;
-
-    afterEach(() => Car.remove({}));
-    beforeEach(() => {
-      return new Car({
-        make: 'hello world',
-        model: 'lsakjf laksjf lkajsdf lkjasflkjasf',
-        year: 2000,
-      })
-      .save()
-      .then(car => {
-        tempCar = car;
-      });
+  describe('Testing GET /api/cars:id', () => {
+    it('Should respond with a cars', () => {
+      return mockCar.createOne()
+        .then(cars => {
+          tempCar = cars;
+          return superagent.get(`${API_URL}/api/cars/${tempCar._id}`);
+        })
+        .then(res => {
+          expect(res.status).toEqual(200);
+          expect(res.body._id).toExist();
+          expect(res.body.make).toEqual(tempCar.make);
+          expect(res.body.model).toEqual(tempCar.model);
+          expect(res.body.year).toEqual(tempCar.year);
+          expect(res.body.drivers).toEqual([]);
+        });
     });
-
-    it('should respond with a car', () => {
-      console.log('tempCar', tempCar);
-      return superagent.get(`${API_URL}/api/cars/${tempCar._id}`)
-      .then(res => {
-        expect(res.status).toEqual(200);
-        expect(res.body._id).toEqual(tempCar._id);
-        expect(res.body.make).toEqual(tempCar.make);
-        expect(res.body.model).toEqual(tempCar.model);
-        expect(res.body.year).toEqual(tempCar.year);
-      });
+    it('should respond with a 404 status', () => {
+      return mockCar.createOne()
+       .then(car => {
+         tempCar = car;
+         return superagent.get(`${API_URL}/api/cars/`);
+       })
+       .catch(res => {
+         expect(res.status).toEqual(404);
+       });
     });
   });
 
   describe('testing PUT /api/cars/:id', () => {
-    afterEach(() => Car.remove({}));
+    afterEach(clearDB);
     beforeEach(() => {
-      return new Car({
-        make: 'hello world',
-        model: 'lsakjf laksjf lkajsdf lkjasflkjasf',
-        year: 2000,
-      })
-      .save()
+      return mockCar.createOne()
       .then(car => {
         tempCar = car;
       });
     });
-
-    it('should respond with a car', () => {
-      console.log('tempCar', tempCar);
+    it('should respond with an updated car and 200 status', () => {
       return superagent.put(`${API_URL}/api/cars/${tempCar._id}`)
-      .send({make: 'hello this is cool'})
+      .send({make: 'Chrysler'})
       .then(res => {
         expect(res.status).toEqual(200);
-        expect(res.body._id).toEqual(tempCar._id);
-        expect(res.body.make).toEqual('hello this is cool');
+        expect(res.body._id).toExist();
+        expect(res.body.make).toEqual('Chrysler');
         expect(res.body.model).toEqual(tempCar.model);
         expect(res.body.year).toEqual(tempCar.year);
+        expect(res.body.drivers).toEqual([]);
       });
     });
-
-    it('should respond with a car', () => {
-      console.log('tempCar', tempCar);
+    it('Should respond with a 400 status code', () => {
       return superagent.put(`${API_URL}/api/cars/${tempCar._id}`)
-      .send({model: 'hehe'})
+          .send({make:''})
+          .catch(res => {
+            expect(res.status).toEqual(400);
+          });
+    });
+    it('should respond with a 404 status', () => {
+      return superagent.put(`${API_URL}/api/cars/1328472`)
+      .send({make: 'Range Rover'})
       .catch(res => {
-        expect(res.status).toEqual(400);
+        expect(res.status).toEqual(404);
       });
     });
   });
 
-  describe('testing DELETE /api/cars/:id', () => {
-    afterEach(() => Car.remove({}));
-    beforeEach(() => {
-      return new Car({
-        make: 'hello world',
-        model: 'lsakjf laksjf lkajsdf lkjasflkjasf',
-        year: 2000,
+  describe('Testing DELETE /api/cars:id', () => {
+    it('Should remove a car', () => {
+      return mockCar.createOne()
+      .then(cars => {
+        tempCar = cars;
+        return superagent.delete(`${API_URL}/api/cars/${tempCar._id}`);
       })
-      .save()
-      .then(note => {
-        tempCar = note;
-      });
-    });
-
-    it('should delete a car', () => {
-      console.log('tempCar', tempCar);
-      return superagent.delete(`${API_URL}/api/cars/${tempCar._id}`)
       .then(res => {
         expect(res.status).toEqual(204);
       });
     });
 
-    it('bad id should respond with a 404', () => {
-      console.log('tempCar', tempCar);
-      return superagent.delete(`${API_URL}/api/cars/12134`)
-      .catch(res => {
-        expect(res.status).toEqual(404);
-      });
-    });
-    it('bad id should respond with a 404', () => {
-      console.log('tempCar', tempCar);
-      return superagent.delete(`${API_URL}/api/cars/5952a8d5c1b8d566a64ea23f`)
+    it('Should respond with a 404', () => {
+      return superagent.delete(`${API_URL}/api/cars/`)
       .catch(res => {
         expect(res.status).toEqual(404);
       });
