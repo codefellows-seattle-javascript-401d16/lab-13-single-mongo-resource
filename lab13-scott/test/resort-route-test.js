1373e9e678577a306a175558866986aad5eeed70
Scott <sdmccoy@gmail.com>
2017-06-28 21:09:14 -0700

    GET test is built.

--- lab13-scott/test/resort-route-test.js
+++ lab13-scott/test/resort-route-test.js
@@ -47,6 +47,44 @@ describe('Testing for /api/resort routes', () => {
     });
   });
 
+  describe('Testing GET routes at /api/lists/:id', () => {
+    describe('If successful', () => {
+      it('it should create a new resort and respond with 200', () => {
+        let tempResort;
+        return mockResort.createOne()
+        .then(resort => {
+          tempResort = resort;
+          return superagent.get(`${API_URL}/api/resorts/${tempResort._id}`);
+        })
+        .then(res => {
+          console.log('res.body: ', res.body);
+          expect(res.status).toEqual(200);
+          expect(res.body.name).toExist(tempResort.name);
+          expect(res.bosy.trails).toEqual([]);
+          expect(res.body._id).toEqual(tempResort._id);
+        });
+      });
+    });
+    describe('If passing in bad content', () => {
+      it('it should respond with 400 status', () => {
+        return superagent.get(`${API_URL}/api/resorts`)
+        .send('bad content')
+        .catch(res => {
+          expect(res.status).toEqual(400);
+        });
+      });
+    });
+    describe('If passing in a bad pathname', () => {
+      it('it should respond with 404 status', () => {
+        return superagent.get(`${API_URL}/api/notapath`)
+        .send({name: `${faker.hacker.verb()} pass resort`})
+        .catch(res => {
+          expect(res.status).toEqual(404);
+        });
+      });
+    });
+  });
+
 
 
 
